8ff3a88e83a96b98fd62177bbd51c8299f80f9d2
Md Azmi Siddique <md.azmi.siddique@gmail.com>
2024-03-17 11:48:58 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,52 @@
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Shop from "./pages/Shop";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/shop/:id",
+        element: <Shop />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      }
+    ]
+  }
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
